7de50294c40cedbc40f5c502cc45b73129644146
RudinaBulliqi <rudinabulliqi@email.com>
2025-05-18 20:39:16 +0200

    Klasat fillestare

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,16 @@
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.net.Socket;
+import java.security.*;
+import java.util.Scanner;
+
 public class Client {
+    private static SecretKey aesKey;
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,18 @@
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.net.*;
+import java.security.*;
+import java.util.Scanner;
+
 public class Server {
+    private static SecretKey aesKey;
+    private static ObjectOutputStream out;
+    private static ObjectInputStream in;
+    private static Socket socket;
+    private static KeyPair rsaKeyPair;
+
+    public static void main(String[] args) throws Exception {
+
+    }
 }
